070683a8231ff1919b74a96ec5dd2e06760a388f
Mj <jhynzar@gmail.com>
2018-08-15 19:14:59 +0800

    Added CostSummaryController and fixed route for it

--- routes/web.php
+++ routes/web.php
@@ -71,9 +71,7 @@ Route::get('Engineer/Home', function(){
 
 Route::get('Engineer/Engineer-Projects','EngineerProjectsController@index');
 
-Route::get('/Engineer/Engineer-Projects/{id}/Cost-Summary',function(){
-    return view('Engineer/cost-summary');
-});
+Route::get('/Engineer/Engineer-Projects/{id}/Cost-Summary','CostSummaryController@index');
 
 Route::get('/Engineer/Engineer-Projects/{id}/Actuals','ActualsController@index');
 //creating actuals
