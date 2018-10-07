eeba1cc82d5dd895bd6fe120950c71fe855e11b1
Mj <jhynzar@gmail.com>
2018-10-07 18:08:25 +0800

    [Admin Dashboard Bugfix] Only compute overhead profit of finished projects

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -46,9 +46,13 @@ class AdminHomeController extends Controller
         $date_next_year = new \DateTime('next year midnight');
         
         $date_max = new \DateTime('first day of January'.$date_next_year->format('Y'));
-        
+
+
+
+        //don't check if active or not, regardless it is still a profit if it's finished
         $projectsFinishedThisYear = DB::table('tblproject')
                         ->join('tblprojectrequirements','tblproject.intProjectId','=','tblprojectrequirements.intProjectId')
+                        ->where('tblproject.strProjectStatus','=','finished')//TODO OVERHEAD PROFIT
                         ->where('tblproject.dtmDateFinished','>=',$date_min->format('Y-m-d'))
                         ->where('tblproject.dtmDateFinished','<',$date_max->format('Y-m-d'))
                         ->where('tblprojectrequirements.strDesc','=','OverheadProfit')//TODO OVERHEAD PROFIT
