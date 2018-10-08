54735625ae760a4274e1ab529c801600beb59a99
Mj <jhynzar@gmail.com>
2018-10-08 16:09:12 +0800

    [Reports] Engineer Projects Sample

--- routes/web.php
+++ routes/web.php
@@ -103,6 +103,8 @@ Route::middleware(['auth'])->group(function(){
 
         Route::get('Engineer/Engineer-Projects','EngineerProjectsController@index');
 
+        Route::get('Engineer/Engineer-Projects/Reports','EngineerProjectsController@reports');
+
         Route::get('/Engineer/Engineer-Projects/{id}/Cost-Summary','CostSummaryController@index');
 
         Route::get('/Engineer/Engineer-Projects/{id}/Actuals','ActualsController@index');
